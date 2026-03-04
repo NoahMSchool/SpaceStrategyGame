d48c8b2b5437297cf94d0b782898003e7b2c1226
NoahMSchool <19noahm@hollandparkschool.co.uk>
2026-03-04 22:07:15 +0000

    Merged Commanders into explorer

--- explorercommander.gd
+++ explorercommander.gd
@@ -1,14 +1,25 @@
 extends Node3D
 
 var orbit_sens = 0.0025
+var pan_sens = 0.001
 var last_mouse_pos := Vector2.ZERO
-
-var current_system = null
 
 @onready var cam : Camera3D = $Camera3D
 
+#enum commander_modes{
+#	EXPLORER,
+#	BIRDSEYE
+#}
+#var commander_style := commander_modes.BIRDSEYE
+var birdseyecommander := true:
+	set(value):
+		birdseyecommander = value
+		update_commander_cam()
+
+var current_system = null
+
 func _ready() -> void:
-	cam.projection = Camera3D.PROJECTION_PERSPECTIVE
+	update_commander_cam()
 	await get_tree().process_frame
 	for s in get_tree().get_nodes_in_group("Selectable"):
 		s.connect("selected", select_system)
@@ -18,12 +29,32 @@ func _process(delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
 	var delta_mouse = mouse_pos - last_mouse_pos
 	last_mouse_pos = mouse_pos
-		
-	if Input.is_action_pressed("RightMouse"):
-		rotation.y -= delta_mouse.x*orbit_sens
-		rotation.x -= delta_mouse.y*orbit_sens
-		rotation.x = clamp(rotation.x, -3*PI/4, 3*PI/8)
+	if birdseyecommander:
+		if Input.is_action_pressed("RightMouse"):
+			global_position.z -= delta_mouse.y*pan_sens*cam.size
+			global_position.x -= delta_mouse.x*pan_sens*cam.size
+		if Input.is_action_just_pressed("ScrollUp"):
+			cam.size+=1
+			cam.size = clampi(cam.size, 5, 50)
+		if Input.is_action_just_pressed("ScrollDown"):
+			cam.size-=1
+			cam.size = clampi(cam.size, 1, 50)
+		if Input.is_action_just_pressed("MouseWheelClick"):
+			cam.size = 25
+	else:
+		if Input.is_action_pressed("RightMouse"):
+			rotation.y -= delta_mouse.x*orbit_sens
+			rotation.x -= delta_mouse.y*orbit_sens
+			rotation.x = clamp(rotation.x, -3*PI/4, 3*PI/8)
 
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("s"):
+		if birdseyecommander:
+			birdseyecommander = false
+		else:
+			birdseyecommander = true
+
+		
 func move_system(system_node : Node3D):
 	print("from : ", current_system)
 	print("to : ", system_node)
@@ -31,11 +62,27 @@ func move_system(system_node : Node3D):
 	if current_system:
 		current_system.system_active = false
 	current_system = system_node
-	var system_viewpoint = system_node.get_node_or_null("ViewPoint")
 	system_node.system_active = true
+	if birdseyecommander:
+		pass
+	else:
+		to_current_system_viewpoint()
+
+func to_current_system_viewpoint():
+	var system_viewpoint = current_system.get_node_or_null("ViewPoint")
 	if system_viewpoint:
 		global_position = system_viewpoint.global_position
-	
 
 func select_system(system : Node3D):
 	move_system(system)
+	
+
+func update_commander_cam():
+	if birdseyecommander:
+		cam.projection = Camera3D.PROJECTION_ORTHOGONAL
+		cam.size = 25
+		global_position.y = 15
+		rotation.x = -90
+	else:
+		cam.projection = Camera3D.PROJECTION_PERSPECTIVE
+		to_current_system_viewpoint()
