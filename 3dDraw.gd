13748025a891b1a4f130a77ae2a91503e6f1f3b9
NoahMSchool <19noahm@hollandparkschool.co.uk>
2026-06-21 08:43:32 +0100

    added 3DDraw line

---
+++ 3dDraw.gd
@@ -0,0 +1,27 @@
+extends Node
+class_name Draw3D
+
+const CONTAINER_PATH = ""
+
+func draw_point(position : Vector3, size : float, resolution):
+	var new_mesh = MeshInstance3D.new()
+	
+
+func draw_line(pos1: Vector3, pos2: Vector3, color = Color.WHITE_SMOKE):
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(pos1)
+	immediate_mesh.surface_add_vertex(pos2)
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = color
+	
+	#get_tree().get_root().
+	add_child(mesh_instance)
--- Galaxy/galaxy_generator.gd
+++ Galaxy/galaxy_generator.gd
@@ -5,9 +5,9 @@ const SYSTEM = preload("res://SolarSystem/solar_system.tscn")
 
 #in lightyears, typical solar systems are 5-10 apart
 const min_system_separation = 7.5
-const max_system_count = 10
+const max_system_count = 15
 
-const galaxy_radius = 100
+#const galaxy_radius = 100
 const disc_height = 10
 
 var systems : Array[SolarSystem] = []
@@ -49,21 +49,23 @@ func _ready() -> void:
 		var attempt = 0
 		while not found:
 			attempt +=1
-			distance +=randf_range(0,min_system_separation)*attempt
+			distance +=randf_range(0,min_system_separation)#*attempt
 			pos = dir*distance
+			pos += Vector3(0,height,0)#put this later to not factor height into distance checks
 			found = true
 			for sp in system_positions:
 				if pos.distance_to(sp) < min_system_separation:
 					found = false
 					break
 				
-		pos += Vector3(0,height,0)
 		system_positions.append(pos)
 	
+	print(system_positions.size())
 	for sp in system_positions:
 		var new_system = SYSTEM.instantiate()
 		add_child(new_system)
 		new_system.position = sp
+		new_system.rotation = Vector3.ZERO
 		systems.append(new_system)
 	
 	algo.set_neighbor_filter_enabled(true)
@@ -75,7 +77,10 @@ func _ready() -> void:
 		for sys2 in systems:
 			if sys1 != sys2:
 				algo.connect_points(sys1.get_instance_id(), sys2.get_instance_id())
-	var path = algo.get_id_path(systems[0].get_instance_id(), systems[20].get_instance_id())
+				if sys1.position.distance_to(sys2.position)<15:
+					_3dDraw.draw_line(sys1.position,sys2.position)
+	var path = algo.get_id_path(systems[0].get_instance_id(), systems[2].get_instance_id())
+	
 	print(path)
 func _process(delta: float) -> void:
 	pass
